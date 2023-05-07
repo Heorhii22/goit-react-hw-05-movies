1d2268ee89693525352bd2cc36addc000e8958b2
George Artemov <oscurasgeorge@gmail.com>
2023-05-07 15:25:28 +0100

    test deploy

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes, Link } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { Home } from '../../pages/Home/Home';
 import { Movies } from '../../pages/Movies/Movies';
 import { MovieInfo } from '../MovieInfo/MovieInfo';
@@ -12,6 +12,7 @@ export const App = () => {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
+          <Route path="/goit-react-hw-05-movies" element={<Home />} />
           <Route path="movies" element={<Movies />} />
           <Route path="movies/:id" element={<MovieInfo />}>
             <Route path="cast" element={<Cast />} />
